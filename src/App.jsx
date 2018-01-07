e4c2699907c8428f7847625cb96c93b4a1654870
Edward Melkoyev <edmelkoyev@gmail.com>
2018-01-07 13:32:57 +0300

    add Switch and not Found

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Toolbar from './components/Toolbar';
 import Content from './components/Content';
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Books from './pages/Books';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
     render() {
@@ -16,10 +17,13 @@ class App extends Component {
                     <Toolbar />
                     
                     <Content>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/about" component={About} />
-                        <Route path="/books" component={Books} />
-                        <Route path="/login" component={Login} />
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/about" component={About} />
+                            <Route path="/books" component={Books} />
+                            <Route path="/login" component={Login} />
+                            <Route component={NotFound} />
+                        </Switch> 
                     </Content>
                 </div>
             </Router>
